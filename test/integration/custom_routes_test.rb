c73d4d8db37be87b575390d5a6625319ceee602d
Elaine Ryan <mselaineryan@gmail.com>
2014-03-17 10:08:01 -1000

    Updated profile and added scoped status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -16,4 +16,10 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
     	get '/register'
     	assert_response :success
     end
+
+    test "that a profile page works" do
+        get '/mselaineryan'
+        assert_response :success
+    end
+
 end
